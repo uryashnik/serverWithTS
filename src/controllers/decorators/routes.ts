f7c3f90b58409a349e248beebb0e91a247d64b29
dd310791uvv <Dsf3f3f3@@>
2022-01-26 19:18:47 +0200

    add method for queries

--- src/controllers/decorators/routes.ts
+++ src/controllers/decorators/routes.ts
@@ -1,8 +1,27 @@
 import 'reflect-metadata';
+import {Methods} from './Methods';
+import {MetadataKeys} from './MetadataKeys';
 
-export function get(path: string){
-    return function(target: any, key: string, desc: PropertyDescriptor){
-        desc.enumerable = true;
-        Reflect.defineMetadata('path', path, target, key);
+// export function post(path: string){
+//     return function(target: any, key: string, desc: PropertyDescriptor){
+//         desc.enumerable = true;
+//         Reflect.defineMetadata('path', path, target, key);
+//         Reflect.defineMetadata('method', 'post', target, key);
+//     }
+// }
+
+function routeBinder(method: string){
+    return function (path: string){
+        return function(target: any, key: string, desc: PropertyDescriptor){
+            desc.enumerable = true;
+            Reflect.defineMetadata(MetadataKeys.path, path, target, key);
+            Reflect.defineMetadata(MetadataKeys.method, method, target, key);
+        }
     }
 }
+
+export const get = routeBinder(Methods.get);
+export const post = routeBinder(Methods.post);
+export const put = routeBinder(Methods.put);
+export const del = routeBinder(Methods.del);
+export const patch = routeBinder(Methods.patch);
--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,14 @@ import cookieSession from 'cookie-session';
 import 'reflect-metadata';
 
 import './controllers/loginController';
-import {router as controllerRouter} from './controllers/decorators/controller'
+import {AppRouter} from "./AppRouter";
 
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
 app.use(cookieSession({keys: ['dfgwetrvbrtbed']}));
 app.use(router);
-app.use(controllerRouter);
+app.use(AppRouter.getInstance());
 
 app.listen(3000, () => {
     console.log('Listening on port 3000');
